63092bec52650aa672c3a82ae306923e1bd46e5d
Rutger Cappendijk <rutgercappendijk@gmail.com>
2024-06-03 10:27:14 +0200

    added first test

--- src/train.py
+++ src/train.py
@@ -1,6 +1,14 @@
+from dataclasses import dataclass
+from typing import Sequence
 import pandas as pd
 import sys
 import os
+
+
+@dataclass
+class PriceMileagePair:
+    price: int
+    mileage: int
 
 
 def read_file(path_to_training_data: str) -> pd.DataFrame:
@@ -10,6 +18,19 @@ def read_file(path_to_training_data: str) -> pd.DataFrame:
         print("File not found")
         exit(1)
     return df
+
+
+def csv_to_pairs(df: pd.DataFrame) -> Sequence[PriceMileagePair]:
+    try:
+        pairs = [PriceMileagePair(row["price"], row["km"]) for _, row in df.iterrows()]
+    except KeyError:
+        print(
+            "Invalid file contents. Ensure that the file contains columns 'km' and 'price'."
+        )
+        exit(1)
+    return pairs
+
+
 
 
 def main():
@@ -22,6 +43,8 @@ def main():
         print("Invalid file format. Only CSV files are supported.")
         exit(1)
     df = read_file(path_to_training_data)
+    pairs = csv_to_pairs(df)
+
     weights_dir = "weights"
     if not os.path.exists(weights_dir):
         os.makedirs(weights_dir)
@@ -29,7 +52,6 @@ def main():
     with open(weights_file, "w") as f:
         f.write("theta0\n")
         f.write("theta1\n")
-
 
 
 if __name__ == "__main__":
---
+++ test/test_train.py
@@ -0,0 +1,6 @@
+from src.train import PriceMileagePair
+
+
+def test_something() -> None:
+    pairs = [PriceMileagePair(1, 2), PriceMileagePair(3, 4)]
+    assert True
